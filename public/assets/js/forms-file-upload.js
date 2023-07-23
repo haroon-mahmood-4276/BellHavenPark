8503d559122bc78d0f18a4b2e97cd9d679c79d64
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2023-07-23 15:04:38 +0500

    BHP-5: Theme Updated

--- public/assets/js/forms-file-upload.js
+++ public/assets/js/forms-file-upload.js
@@ -1,4 +1,4 @@
-"use strict";!function(){var a=`<div class="dz-preview dz-file-preview">
+"use strict";!function(){var e=`<div class="dz-preview dz-file-preview">
 <div class="dz-details">
   <div class="dz-thumbnail">
     <img data-dz-thumbnail>
@@ -13,4 +13,4 @@
   <div class="dz-filename" data-dz-name></div>
   <div class="dz-size" data-dz-size></div>
 </div>
-</div>`;new Dropzone("#dropzone-basic",{previewTemplate:a,parallelUploads:1,maxFilesize:5,addRemoveLinks:!0,maxFiles:1}),new Dropzone("#dropzone-multi",{previewTemplate:a,parallelUploads:1,maxFilesize:5,addRemoveLinks:!0})}();
+</div>`,a=document.querySelector("#dropzone-basic"),a=(a&&new Dropzone(a,{previewTemplate:e,parallelUploads:1,maxFilesize:5,addRemoveLinks:!0,maxFiles:1}),document.querySelector("#dropzone-multi"));a&&new Dropzone(a,{previewTemplate:e,parallelUploads:1,maxFilesize:5,addRemoveLinks:!0})}();
